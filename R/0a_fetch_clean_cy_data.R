3fd186ea3d745001caba85ca6dd91abf2d6e1577
mibahia <mariaizabel.bahia@gmail.com>
2025-08-01 10:29:00 +0100

    lint apply to first cleaning step

--- R/0a_fetch_clean_cy_data.R
+++ R/0a_fetch_clean_cy_data.R
@@ -5,17 +5,33 @@ source("R/functions/clean_calendar_year_births_lad_1993_2001.R")
 source("R/functions/clean_calendar_year_births_lad_2022_2023.R")
 source("R/functions/split_city_of_london_isles_of_scilly.R")
 
-fpath <- list(raw_births_lad_cy1993_2021 = "data/raw/birthsbyageofmotherlaua19932021finaltable.xlsx",
-              raw_births_lad_cy2022_2023 = "data/raw/finalfileage.xlsx",
-              births_lad_cy = "data/intermediate/births_lad_cy.rds"
-              )
+fpath <- list(
+  raw_births_lad_cy1993_2021 =
+    "data/raw/birthsbyageofmotherlaua19932021finaltable.xlsx",
+  raw_births_lad_cy2022_2023 =
+    "data/raw/finalfileage.xlsx",
+  births_lad_cy =
+    "data/intermediate/births_lad_cy.rds"
+)
 
-urls <- list(births_lad_cy_1993_2021 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/adhocs/1265livebirthsbyageofmotherandlocalauthorityenglandandwales1993to2021/birthsbyageofmotherlaua19932021finaltable.xlsx",
-             births_lad_cy_2022_2023 = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/conceptionandfertilityrates/adhocs/2609livebirthsbyageofmotherbylocalauthoritiesenglandandwales2022to2023/finalfileage.xlsx")
+urls <- list(
+  births_lad_cy_1993_2021 =
+    "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/" %>%
+    paste0("birthsdeathsandmarriages/livebirths/adhocs/",
+           "1265livebirthsbyageofmotherandlocalauthorityenglandandwales",
+           "1993to2021/birthsbyageofmotherlaua19932021finaltable.xlsx"),
+
+  births_lad_cy_2022_2023 =
+    "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/" %>%
+    paste0("birthsdeathsandmarriages/conceptionandfertilityrates/adhocs/",
+           "2609livebirthsbyageofmotherbylocalauthoritiesenglandandwales",
+           "2022to2023/finalfileage.xlsx")
+)
 
 
-if(!dir.exists("data/raw/")) dir.create("data/raw/", recursive = TRUE)
-if(!dir.exists("data/intermediate/")) dir.create("data/intermediate/", recursive = TRUE)
+if (!dir.exists("data/raw/")) dir.create("data/raw/", recursive = TRUE)
+if (!dir.exists("data/intermediate/"))
+  dir.create("data/intermediate/", recursive = TRUE)
 
 # calendar lad data
 
@@ -27,22 +43,33 @@ download.file(url = urls$births_lad_cy_2022_2023,
               destfile = fpath$raw_births_lad_cy2022_2023,
               mode = "wb")
 
-births_lad_cy1993_2021_21codes <- clean_calendar_year_births_lad_1993_2001(fpath$raw_births_lad_cy1993_2021) %>%
+# Read, clean and recode births per (LAD - 1993-2021)
+births_lad_cy1993_2021 <- clean_cy_births_lad_1993_2001(
+  fpath$raw_births_lad_cy1993_2021
+) %>%
   select(-gss_name)
 
-births_lad_cy1993_2021 <- recode_gss(births_lad_cy1993_2021_21codes,
+births_lad_cy1993_2021 <- recode_gss(births_lad_cy1993_2021,
                                      recode_from_year = 2021,
                                      recode_to_year = 2023) %>%
   add_gss_names(gss_year = 2023)
 
-births_lad_cy2022_2023_combined_areas <- clean_calendar_year_births_lad_2022_2023(fpath$raw_births_lad_cy2022_2023)
 
-births_lad_cy2022_2023 <- split_city_of_london_isles_of_scilly(data_to_split = births_lad_cy2022_2023_combined_areas,
-                                                               past_split_data = births_lad_cy1993_2021,
-                                                               num_years_past_data = 10)
+# Read, clean and split combined areas (LAD - from 2022)
+births_lad_cy2022_2023 <- clean_cy_births_lad_2022_2023(
+  fpath$raw_births_lad_cy2022_2023
+)
 
+births_lad_cy2022_2023 <- split_city_of_london_isles_of_scilly(
+  data_to_split = births_lad_cy2022_2023,
+  past_split_data = births_lad_cy1993_2021,
+  num_years_past_data = 10
+)
+
+# Combine both datasets (LAD - from 1993)
 births_lad_cy <- bind_rows(births_lad_cy1993_2021, births_lad_cy2022_2023)
 
-rm(births_lad_cy1993_2021_21codes, births_lad_cy1993_2021, births_lad_cy2022_2023_combined_areas, births_lad_cy2022_2023)
+rm(births_lad_cy1993_2021,
+   births_lad_cy2022_2023)
 
 saveRDS(births_lad_cy, fpath$births_lad_cy)
