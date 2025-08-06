2b1c574b374f076a5fae85f16fb3621a3ea528fe
mibahia <mariaizabel.bahia@gmail.com>
2025-08-06 11:13:03 +0100

    births per cy refactored clean step

--- R/0a_fetch_clean_cy_data.R
+++ R/0a_fetch_clean_cy_data.R
@@ -1,75 +1,88 @@
 library(dplyr)
 library(gsscoder)
 
-source("R/functions/clean_calendar_year_births_lad_1993_2001.R")
-source("R/functions/clean_calendar_year_births_lad_2022_2023.R")
-source("R/functions/split_city_of_london_isles_of_scilly.R")
+source("R/functions/read_and_clean_calendar_year_births.R")
+source("R/functions/split_joint_lads.R")
 
-fpath <- list(
+
+###### Step 1: Create global variables ######
+
+file_path <- list(
   raw_births_lad_cy1993_2021 =
-    "data/raw/birthsbyageofmotherlaua19932021finaltable.xlsx",
+    "data/raw/births_by_ageofmother_lad_1993_2021.xlsx",
   raw_births_lad_cy2022_2023 =
-    "data/raw/finalfileage.xlsx",
+    "data/raw/births_by_ageofmother_lad_2022_2023.xlsx",
   births_lad_cy =
     "data/intermediate/births_lad_cy.rds"
 )
 
 urls <- list(
-  births_lad_cy_1993_2021 = paste0(
-    "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/",
-    "birthsdeathsandmarriages/livebirths/adhocs/",
-    "1265livebirthsbyageofmotherandlocalauthorityenglandandwales",
-    "1993to2021/birthsbyageofmotherlaua19932021finaltable.xlsx"),
+  births_lad_cy1993_2021 =
+    paste0(
+           "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/",
+           "birthsdeathsandmarriages/livebirths/adhocs/",
+           "1265livebirthsbyageofmotherandlocalauthorityenglandandwales",
+           "1993to2021/birthsbyageofmotherlaua19932021finaltable.xlsx"),
 
-  births_lad_cy_2022_2023 = paste0(
-    "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/",
-    "birthsdeathsandmarriages/conceptionandfertilityrates/adhocs/",
-    "2609livebirthsbyageofmotherbylocalauthoritiesenglandandwales",
-    "2022to2023/finalfileage.xlsx")
+  births_lad_cy2022_2023 =
+    paste0(
+           "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/",
+           "birthsdeathsandmarriages/conceptionandfertilityrates/adhocs/",
+           "2609livebirthsbyageofmotherbylocalauthoritiesenglandandwales",
+           "2022to2023/finalfileage.xlsx")
 )
 
-
 if (!dir.exists("data/raw/")) dir.create("data/raw/", recursive = TRUE)
-if (!dir.exists("data/intermediate/"))
-  dir.create("data/intermediate/", recursive = TRUE)
+if (!dir.exists("data/intermediate/")) dir.create("data/intermediate/",
+                                                  recursive = TRUE)
 
-# calendar lad data
 
-download.file(url = urls$births_lad_cy_1993_2021,
-              destfile = fpath$raw_births_lad_cy1993_2021,
+###### Step 2: Download calendar year LAD data from ONS ######
+
+download.file(url = urls$births_lad_cy1993_2021,
+              destfile = file_path$raw_births_lad_cy1993_2021,
               mode = "wb")
 
-download.file(url = urls$births_lad_cy_2022_2023,
-              destfile = fpath$raw_births_lad_cy2022_2023,
+download.file(url = urls$births_lad_cy2022_2023,
+              destfile = file_path$raw_births_lad_cy2022_2023,
               mode = "wb")
 
-# Read, clean and recode births per (LAD - 1993-2021)
-births_lad_cy1993_2021 <- clean_cy_births_lad_1993_2001(
-  fpath$raw_births_lad_cy1993_2021
+
+# Step 3: Read, clean and re-code births per LAD (1993-2021)
+
+births_lad_cy1993_2021 <- read_and_clean_ms_births_lad(
+  file_path = file_path$raw_births_lad_cy1993_2021
 ) %>%
   select(-gss_name)
 
-births_lad_cy1993_2021 <- recode_gss(births_lad_cy1993_2021,
+births_lad_cy1993_2021 <- recode_gss(df_in = births_lad_cy1993_2021,
                                      recode_from_year = 2021,
                                      recode_to_year = 2023) %>%
   add_gss_names(gss_year = 2023)
 
 
-# Read, clean and split combined areas (LAD - from 2022)
-births_lad_cy2022_2023 <- clean_cy_births_lad_2022_2023(
-  fpath$raw_births_lad_cy2022_2023
+###### Step 4: Read, clean and split combined areas per LAD (from 2022) ######
+# In this case we are splitting City of London & Isles of Scilly
+
+births_lad_cy2022_2023 <- read_and_clean_ss_births_lad(
+  file_path = file_path$raw_births_lad_cy2022_2023
 )
 
-births_lad_cy2022_2023 <- split_city_of_london_isles_of_scilly(
+births_lad_cy2022_2023 <- split_joint_lads(
   data_to_split = births_lad_cy2022_2023,
-  past_split_data = births_lad_cy1993_2021,
+  past_data = births_lad_cy1993_2021,
+  separated_codes = list(
+    c("E09000001", "E09000012"), c("E06000052", "E06000053")),
+  joint_codes = c("E09000001 & E09000012", "E06000052 & E06000053"),
   num_years_past_data = 10
 )
 
-# Combine both datasets (LAD - from 1993)
+
+###### Step 5: Combine and save births at LAD level from 1993 ######
+
 births_lad_cy <- bind_rows(births_lad_cy1993_2021, births_lad_cy2022_2023)
 
 rm(births_lad_cy1993_2021,
    births_lad_cy2022_2023)
 
-saveRDS(births_lad_cy, fpath$births_lad_cy)
+saveRDS(births_lad_cy, file_path$births_lad_cy)
