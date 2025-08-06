2ebc5b70b11312353bc49cccd505888a2e916caf
mibahia <mariaizabel.bahia@gmail.com>
2025-08-06 12:33:28 +0100

    fix typo

--- R/functions/split_joint_lads.R
+++ R/functions/split_joint_lads.R
@@ -8,12 +8,13 @@ library(dplyr)
 #' @param joint_codes A character vector.
 #' @param num_years_past_data A integer / double.
 #' @returns A data frame.
-split_joint_lads <- function(data_to_split,
-                             past_data,
-                             separated_codes,
-                             joint_codes,
-                             num_years_past_data) {
-
+split_joint_lads <- function(
+  data_to_split,
+  past_data,
+  separated_codes,
+  joint_codes,
+  num_years_past_data
+) {
   for (i in joint_codes) {
     tryCatch({
       if (!(i %in% births_lad_cy2022_2023$gss_code)) {
@@ -30,34 +31,41 @@ split_joint_lads <- function(data_to_split,
   temporary_list <- sapply(joint_codes, function(x) NULL)
 
   for (i in 1:length(joint_codes)) {
-    temporary_list[[i]] <- get_proportion_and_joint_codes(past_data,
-                                                          separated_codes[[i]],
-                                                          joint_codes[i],
-                                                          num_years_past_data)
+    temporary_list[[i]] <- get_proportion_and_joint_codes(
+      past_data,
+      separated_codes[[i]],
+      joint_codes[i],
+      num_years_past_data
+    )
   }
 
   proportions_by_age_of_mother <- bind_rows(temporary_list)
 
-  separated_las <- split_combined_la(data_to_split,
-                                    proportions_by_age_of_mother,
-                                    joint_codes,
-                                    lookup_names)
+  separated_las <- split_combined_la(
+    data_to_split,
+    proportions_by_age_of_mother,
+    joint_codes,
+    lookup_names
+  )
 
   return(separated_las)
 }
 
 
-split_combined_la <- function(data_to_split,
-                              proportions_by_age_of_mother,
-                              joint_codes,
-                              lookup_names) {
-
+split_combined_la <- function(
+  data_to_split,
+  proportions_by_age_of_mother,
+  joint_codes,
+  lookup_names
+) {
   split_combined_la <- data_to_split %>%
     filter(gss_code %in% joint_codes) %>%
     rename(joint_code = gss_code) %>%
-    full_join(proportions_by_age_of_mother,
-              by = c("joint_code", "age_of_mother"),
-              relationship = "many-to-many") %>%
+    full_join(
+      proportions_by_age_of_mother,
+      by = c("joint_code", "age_of_mother"),
+      relationship = "many-to-many"
+    ) %>%
     mutate(value = round(value * proportion, 0)) %>%
     select(-c(proportion, joint_code, gss_name)) %>%
     left_join(lookup_names, by = "gss_code")
@@ -68,25 +76,24 @@ split_combined_la <- function(data_to_split,
     arrange(year, gss_code, age_of_mother)
 
   return(merge_split_la_data)
-
 }
 
 
-get_proportion_and_joint_codes <- function(past_data,
-                                           separated_codes,
-                                           joint_codes,
-                                           num_years_past_data) {
-
+get_proportion_and_joint_codes <- function(
+  past_data,
+  separated_codes,
+  joint_codes,
+  num_years_past_data
+) {
   proportion_and_joint_code <- past_data %>%
     filter(year > max(year) - num_years_past_data + 1) %>%
-    filter(gss_code %in% separeted_codes) %>%
+    filter(gss_code %in% separated_codes) %>%
     group_by(gss_code, age_of_mother) %>%
     summarise(value = sum(value), .groups = "drop") %>%
     group_by(age_of_mother) %>%
     mutate(total = sum(value)) %>%
     ungroup() %>%
-    mutate(proportion = value / total,
-           joint_code = joint_codes) %>%
+    mutate(proportion = value / total, joint_code = joint_codes) %>%
     select(gss_code, age_of_mother, proportion, joint_code)
 
   return(proportion_and_joint_code)
