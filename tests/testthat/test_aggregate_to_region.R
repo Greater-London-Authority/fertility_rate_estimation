8cd7c175fa6f13afc988099ef9a9b453063fb2c4
mibahia <mariaizabel.bahia@gmail.com>
2025-08-15 14:14:30 +0100

    adding test and test data

---
+++ tests/testthat/test_aggregate_to_region.R
@@ -0,0 +1,21 @@
+source(paste0(
+  "~/git/fertility_rate_estimation/",
+  "R/functions/aggregate_to_region.R"
+))
+
+dir <- "~/git/fertility_rate_estimation/tests/testthat/test_data/"
+
+test_that("xxx", {
+  births_lad_sample <- readRDS(paste0(dir, "births_lad_sample.rds"))
+  expected_output <- readRDS(paste0(
+    dir,
+    "births_lad_sample_expected_output.rds"
+  ))
+
+  output <- aggregate_to_region(
+    births_lad_sample,
+    "~/git/fertility_rate_estimation/lookups/lookup_lad_rgn.rds",
+    "RGN"
+  )
+  expect_identical(output, expected_output)
+})
