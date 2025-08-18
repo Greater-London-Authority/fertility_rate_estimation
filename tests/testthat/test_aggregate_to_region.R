65d7fb58cfd84e979ca958a581f549bc7b84025a
mibahia <mariaizabel.bahia@gmail.com>
2025-08-18 16:52:32 +0100

    adding comment to test

--- tests/testthat/test_aggregate_to_region.R
+++ tests/testthat/test_aggregate_to_region.R
@@ -5,7 +5,9 @@ source(paste0(
 
 dir <- "~/git/fertility_rate_estimation/tests/testthat/test_data/"
 
-test_that("xxx", {
+test_that("`aggregate_to_region` should aggregate
+  the given geography and add that to the existing dataframe", {
+
   births_lad_sample <- readRDS(paste0(dir, "births_lad_sample.rds"))
   expected_output <- readRDS(paste0(
     dir,
